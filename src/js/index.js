14a1efc96bb51cae67fba0d4ce20a2f50a81041e
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-04-22 14:44:39 -0400

    click drop down and get data from item clicked

--- src/js/index.js
+++ src/js/index.js
@@ -74,30 +74,32 @@ function abbrState(input, to) {
   }
 }
 
-// async function getWeather(id) {
-//   try {
-//     const response = await fetch(
-//       `http://api.weatherapi.com/v1/current.json?key=82dab6adbac749db993202938241204&q=id:${id}`
-//     );
-//     const data = await response.json();
-//     if (response.status === 200) {
-//       return data;
-//     }
-//     console.log('Server error:', data.error.message);
-//   } catch (error) {
-//     console.log('Fetch error:', error);
-//   }
-// }
+async function getWeather(id) {
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=08b3b4bc92cf4cc2ac8181252242204&q=id:${id}`
+    );
+    const data = await response.json();
+    if (response.status !== 200) {
+      console.log('Server error:', data.error.message);
+      return false;
+    }
+    return data;
+  } catch (error) {
+    console.log('Fetch error:', error);
+  }
+}
 
 async function getLocation(location = '') {
   const locations = [];
   try {
     const response = await fetch(
-      `http://api.weatherapi.com/v1/search.json?key=82dab6adbac749db993202938241204&q=${location}`
+      `http://api.weatherapi.com/v1/search.json?key=08b3b4bc92cf4cc2ac8181252242204&q=${location}`
     );
     const data = await response.json();
     if (response.status !== 200) {
       console.log('Server error:', data.error.message);
+      return false;
     }
     locations.push(...data);
     return locations;
@@ -107,16 +109,17 @@ async function getLocation(location = '') {
   }
 }
 
-function displayResults(locationArray) {
+function displaySearchResults(locationArray) {
   const domContainer = document.querySelector('.results-container');
   domContainer.textContent = '';
-  console.log(locationArray);
   if (locationArray.length !== 0) {
     const locationList = document.createElement('ul');
     domContainer.appendChild(locationList);
-    locationArray.forEach((location) => {
+    locationArray.forEach((location, index) => {
       const abbr = abbrState(location.region, 'abbr');
       const locationWrapper = document.createElement('li');
+      locationWrapper.classList.add('location-list-item');
+      locationWrapper.dataset.locationIndex = index;
       locationWrapper.textContent = `${location.name}, ${
         abbr !== undefined ? abbr : location.region
       }`;
@@ -125,11 +128,21 @@ function displayResults(locationArray) {
   }
 }
 
+let locations = [];
 const searchInput = document.querySelector('#weatherLocation');
 searchInput.addEventListener('input', (e) => {
   if (e.target.value) {
     getLocation(e.target.value).then((data) => {
-      displayResults(data);
+      locations = [...data];
+      displaySearchResults(data);
     });
+  }
+});
+
+document.addEventListener('click', (e) => {
+  if (e.target.className === 'location-list-item') {
+    getWeather(locations[e.target.dataset.locationIndex].id).then((data) =>
+      console.log(data)
+    );
   }
 });
